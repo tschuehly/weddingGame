4ee1d45c597bb636ec6c4f05025e471256fdcbf8
Thomas Schuehly <thomas.schuehly@outlook.com>
2022-07-07 11:21:29 +0200

    feature: wedding-manager: theme selection ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,6 @@ module.exports = {
     },
   },
   plugins: [
-    require('@tailwindcss/forms'),
     require("daisyui")],
   daisyui: {
     themes: ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"],
